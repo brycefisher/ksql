aeff4f5d06066558644a0b6294a8f8f2e0b116e2
Bryce Fisher-Fleig <brycefisher@users.noreply.github.com>
2022-01-21 18:35:28 +0000

    feat: real timestamps in fake messages

--- fake_cdn_log_producer.py
+++ fake_cdn_log_producer.py
@@ -1,5 +1,7 @@
 import json
+import math
 import time
+from datetime import datetime, timezone
 
 from pykafka import KafkaClient
 
@@ -9,9 +11,10 @@ client = KafkaClient(hosts="localhost:29092")
 topic = client.topics['cdn-logs']
 with topic.get_sync_producer() as producer:
     while True:
+        now = math.floor(datetime.now(timezone.utc).timestamp() * 1000)
         record = {
             "asset_id": 1,
-            "request_begin": 0, # int (unix timestamp in milliseconds)
+            "request_begin": now, # int (unix timestamp in milliseconds)
             "segment_duration": 5, # int (seconds of video content)
             "status_code": 200,
             "environment_id": 1,
